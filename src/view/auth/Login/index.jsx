ff9faac6c17fd46eda86de75cc47d6736978cabb
Nguyen Trung <85296548+nttrung309@users.noreply.github.com>
2024-04-01 01:13:16 +0700

    update sign up page

--- src/view/auth/Login/index.jsx
+++ src/view/auth/Login/index.jsx
@@ -6,40 +6,45 @@ import LogoIcon  from'../../../shared/asset/icon/logo.svg';
 import { Button, Input } from "antd";
 import { GoogleOutlined } from "@ant-design/icons";
 
+import { useNavigate } from "react-router-dom";
+import { routerSignUp } from "../SignUp/router";
+
 const Login = () => {
+    const navigate = useNavigate();
+
     return(
-        <div className="auth-login">
-            <ReactSVG className="auth-login__banner" src={LogoIcon}/>
-            <div className="auth-login__login-form">
-                <div className="auth-login__login-form__container">
-                    <div className="auth-login__login-form__header">
-                        <div className="auth-login__login-form__header__title">
+        <div className="auth">
+            <ReactSVG className="auth__banner" src={LogoIcon}/>
+            <div className="auth__form">
+                <div className="auth__form__container">
+                    <div className="auth__form__header">
+                        <div className="auth__form__header__title">
                             Đăng nhập
                         </div>
-                        <div className="auth-login__login-form__header__sub-title">
+                        <div className="auth__form__header__sub-title">
                             {'Chưa có tài khoản? '}
-                            <span>
+                            <span onClick={() => navigate(routerSignUp.path)}>
                                 Đăng ký ngay
                             </span>
                         </div>
                     </div>
-                    <div className="auth-login__login-form__body">
-                        <div className="auth-login__login-form__body__input-containter">
-                            <div className="auth-login__login-form__input-item">
-                                <div className="auth-login__login-form__input-item__label">Email</div>
-                                <Input className="auth-login__login-form__input-item__input" placeholder="Email" />
+                    <div className="auth__form__body">
+                        <div className="auth__form__body__input-containter">
+                            <div className="auth__form__input-item">
+                                <div className="auth__form__input-item__label">Email</div>
+                                <Input className="auth__form__input-item__input" placeholder="Email" />
                             </div>
-                            <div className="auth-login__login-form__input-item">
-                                <div className="auth-login__login-form__input-item__label">Mật khẩu</div>
-                                <Input.Password className="auth-login__login-form__input-item__input" placeholder="Mật khẩu" />
+                            <div className="auth__form__input-item">
+                                <div className="auth__form__input-item__label">Mật khẩu</div>
+                                <Input.Password className="auth__form__input-item__input" placeholder="Mật khẩu" />
                             </div>
                         </div>
-                        <div className="auth-login__login-form__forgot-password">Quên mật khẩu?</div>
-                        <div className="auth-login__login-form__control-group">
-                            <Button className="auth-login__login-form__btn login" type='primary'>Đăng nhập</Button>
-                            <div className="auth-login__login-form__control-group__divider">Hoặc</div>
+                        <div className="auth__form__forgot-password">Quên mật khẩu?</div>
+                        <div className="auth__form__control-group">
+                            <Button className="auth__form__btn primary" type='primary'>Đăng nhập</Button>
+                            <div className="auth__form__control-group__divider">Hoặc</div>
                                 <Button
-                                    className="auth-login__login-form__btn gg-login"
+                                    className="auth__form__btn gg-login"
                                     icon={<GoogleOutlined/>}
                                 >
                                         Đăng nhập với Google
--- src/view/auth/SignUp/index.jsx
+++ src/view/auth/SignUp/index.jsx
@@ -1,8 +1,62 @@
 import React from "react";
 
+import { ReactSVG } from 'react-svg';
+
+import LogoIcon  from'../../../shared/asset/icon/logo.svg';
+import { Button, Input } from "antd";
+import { GoogleOutlined } from "@ant-design/icons";
+
+import { routerLogin } from "../Login/router";
+import { useNavigate } from "react-router-dom";
+
 const SignUp = () => {
+    const navigate = useNavigate();
+
     return(
-        <div>This's sign up</div>
+        <div className="auth sign-up">
+            <ReactSVG className="auth__banner" src={LogoIcon}/>
+            <div className="auth__form">
+                <div className="auth__form__container">
+                    <div className="auth__form__header">
+                        <div className="auth__form__header__title">
+                            Đăng ký
+                        </div>
+                        <div className="auth__form__header__sub-title">
+                            {'Đã có tài khoản? '}
+                            <span onClick={() => navigate(routerLogin.path)}>
+                                Đăng nhập ngay
+                            </span>
+                        </div>
+                    </div>
+                    <div className="auth__form__body">
+                        <div className="auth__form__body__input-containter">
+                            <div className="auth__form__input-item">
+                                <div className="auth__form__input-item__label">Email</div>
+                                <Input className="auth__form__input-item__input" placeholder="Email" />
+                            </div>
+                            <div className="auth__form__input-item">
+                                <div className="auth__form__input-item__label">Mật khẩu</div>
+                                <Input.Password className="auth__form__input-item__input" placeholder="Mật khẩu" />
+                            </div>
+                            <div className="auth__form__input-item">
+                                <div className="auth__form__input-item__label">Xác nhận mật khẩu</div>
+                                <Input.Password className="auth__form__input-item__input" placeholder="Xác nhận mật khẩu" />
+                            </div>
+                        </div>
+                        <div className="auth__form__control-group" style={{marginTop: '20px'}}>
+                            <Button className="auth__form__btn primary" type='primary'>Đăng ký</Button>
+                            <div className="auth__form__control-group__divider">Hoặc</div>
+                                <Button
+                                    className="auth__form__btn gg-login"
+                                    icon={<GoogleOutlined/>}
+                                >
+                                        Đăng ký với Google
+                                </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
