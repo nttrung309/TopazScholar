6113b6c551e2a38778cd527d5689e7419e76693c
ngtrhh <91332224+ngtrhh@users.noreply.github.com>
2024-04-04 13:48:36 +0700

    up

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+import SideBar from "../SideBar";
 
-import Header from '../Header';
-import Footer from '../Footer';
-import SideBar from '../SideBar';
+const MainLayout = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
 
-const MainLayout = ({children}) => {
-    return(//Tui return để test thôi bà làm thì xóa đi nha
-        <>  
-            <Header/>
-            <SideBar/>
-            {children}
-            <Footer/>
-        </>
-    );
+  const handleCollapsed = (value) => {
+    setCollapsed(value);
+  };
+
+  return (
+    <div className="main-layout">
+      <SideBar collapsed={collapsed} />
+      <div className={`main-container${collapsed ? " collapsed" : ""}`}>
+        <Header collapsed={collapsed} handleCollapsed={handleCollapsed} />
+        {children}
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Home = () => {
-    return(
-        <div>This's home</div>
-    );
-}
+  return <div className="home">This's home</div>;
+};
 
 export default Home;
