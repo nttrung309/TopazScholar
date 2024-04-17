2cc157e532db542e1676673fa0b5f64b59ef92f6
ngtrhh <91332224+ngtrhh@users.noreply.github.com>
2024-04-18 02:32:20 +0700

    add register page

--- src/view/activity/explore/index.jsx
+++ src/view/activity/explore/index.jsx
@@ -30,10 +30,11 @@ const ActivyExplore = () => {
       <div className="main">
         <div className="page-title">
           <div className="title-icon">
-            <i className="bi bi-lightning" />
+            <i className="bi bi-lightning-fill" />
           </div>
-          <div className="title-name">Trang chủ</div>
+          <div className="title-name">Khám phá</div>
         </div>
+
         <div className="row">
           <Dropdown menu={{ items }} trigger={["click"]}>
             <div>
--- src/view/activity/registered/index.jsx
+++ src/view/activity/registered/index.jsx
@@ -1,7 +1,37 @@
+import SideBar from "layout/SideBar";
 import React from "react";
+import ActivityCard from "shared/components/ActivityCard";
+import SubSidebar from "shared/components/SubSidebar";
 
 const ActivyRegistered = () => {
-  return <div className="activity-registered">index</div>;
+  return (
+    <div className="activity-registered">
+      <SubSidebar />
+
+      <div className="main">
+        <div className="page-title">
+          <div className="title-icon">
+            <i className="bi bi-lightning-fill" />
+          </div>
+          <div className="title-name">Hoạt động bạn đã đăng ký tham gia</div>
+        </div>
+
+        <div className="flex-column">
+          <div className="item">
+            <div className="title">Hôm nay</div>
+            <ActivityCard variant="horizontal" />
+            <ActivityCard variant="horizontal" />
+          </div>
+
+          <div className="item">
+            <div className="title">Tháng trước</div>
+            <ActivityCard variant="horizontal" />
+            <ActivityCard variant="horizontal" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ActivyRegistered;
