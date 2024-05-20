5734e2d4378e3ebac4012d004dd2b0664cd0e0a3
ngtrhh <91332224+ngtrhh@users.noreply.github.com>
2024-05-20 16:32:05 +0700

    update sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useEffect, useState } from "react";
 
-import PublicPage from './router/components/PublicPages';
-import PrivatePage from './router/components/PrivatePages';
-import AdminPage from 'router/components/AdminPages';
+import PublicPage from "./router/components/PublicPages";
+import PrivatePage from "./router/components/PrivatePages";
+import AdminPage from "router/components/AdminPages";
 
-import {publicPage, privatePage, adminPage} from './router/mainRouter';
+import { publicPage, privatePage, adminPage } from "./router/mainRouter";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 
-import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation} from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  AuthDataLoadingStateSelector,
+  AuthRoleSelector,
+  StatusLoginSelector,
+} from "./redux/auth/userSelector";
+import { AutoLogin } from "./redux/auth/userThunk";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { AuthDataLoadingStateSelector, AuthRoleSelector, StatusLoginSelector } from './redux/auth/userSelector';
-import { AutoLogin } from './redux/auth/userThunk';
-
-const MainView = memo(()=> {
+const MainView = memo(() => {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
@@ -21,48 +30,66 @@ const MainView = memo(()=> {
   const privateRoutes = privatePage.map((page) => page.path);
   const adminRoutes = adminPage.map((page) => page.path);
   const statusLogin = useSelector(StatusLoginSelector);
-  const authDataLoadingState = useSelector(AuthDataLoadingStateSelector); 
+  const authDataLoadingState = useSelector(AuthDataLoadingStateSelector);
   const authRole = useSelector(AuthRoleSelector);
 
   useEffect(() => {
     GetAuth();
   }, []);
-  
+
   useEffect(() => {
-    if (!statusLogin && (authDataLoadingState === 'succeeded' || authDataLoadingState === 'failed') && !publicRoutes.includes(location.pathname)) {
-      navigate('/auth/login');
+    if (
+      !statusLogin &&
+      (authDataLoadingState === "succeeded" ||
+        authDataLoadingState === "failed") &&
+      !publicRoutes.includes(location.pathname)
+    ) {
+      navigate("/auth/login");
+    } else if (
+      statusLogin &&
+      authRole === "student" &&
+      !privateRoutes.includes(location.pathname)
+    ) {
+      navigate("/");
+    } else if (
+      statusLogin &&
+      authRole === "admin" &&
+      !adminRoutes.includes(location.pathname)
+    ) {
+      navigate("admin/activity");
     }
-    else if(statusLogin && authRole === 'student' && !privateRoutes.includes(location.pathname)){
-        navigate('/');
-    }
-    else if(statusLogin && authRole === 'admin' && !adminRoutes.includes(location.pathname)){
-      navigate('admin/activity');
-    }
+
+    console.log(statusLogin);
+    console.log(authRole);
   }, [authDataLoadingState, authRole, navigate]);
 
   const GetAuth = async () => {
     try {
-        // Dispatch action 'login' với username và password
-        await dispatch(AutoLogin());
+      // Dispatch action 'login' với username và password
+      await dispatch(AutoLogin());
     } catch (error) {
-        // Xử lý lỗi nếu có
-        console.error('Error occurred:', error.message);
+      // Xử lý lỗi nếu có
+      console.error("Error occurred:", error.message);
     }
   };
 
-  return(
+  return (
     <>
-      {(statusLogin && authRole != 'admin') ? <PrivatePage /> : <PublicPage />}
-      {(statusLogin && authRole == 'admin') ? <AdminPage/> : null}
+      {statusLogin && authRole !== "admin" ? (
+        <PrivatePage />
+      ) : authRole === "admin" ? (
+        <AdminPage />
+      ) : (
+        <PublicPage />
+      )}
     </>
   );
 });
 
-
 function App() {
   return (
     <Router>
-      <MainView/>
+      <MainView />
     </Router>
   );
 }
--- src/layout/SideBar/index.jsx
+++ src/layout/SideBar/index.jsx
@@ -1,59 +1,105 @@
+import React, { useEffect, useState } from "react";
 import { ReactSVG } from "react-svg";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 import LogoIcon from "../../shared/asset/icon/logo.svg";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { AuthRoleSelector } from "../../redux/auth/userSelector";
+
+const menus = [
+  {
+    name: "home",
+    label: "Trang chủ",
+    icon: "bi bi-house",
+    iconActive: "bi bi-house-fill",
+    path: "/",
+    role: "student",
+  },
+  {
+    name: "activity",
+    label: "Hoạt động",
+    icon: "bi bi-lightning",
+    iconActive: "bi bi-lightning-fill",
+    path: "/explore",
+    role: "student",
+  },
+  {
+    name: "calendar",
+    label: "Lịch",
+    icon: "bi bi-table",
+    iconActive: "bi bi-table",
+    path: "/calendar",
+    role: "student",
+  },
+  {
+    name: "contact",
+    label: "Trò chuyện",
+    icon: "bi bi-cursor",
+    iconActive: "bi bi-cursor-fill",
+    path: "/contact",
+    role: "student",
+  },
+  {
+    name: "statistic",
+    label: "Thống kê",
+    icon: "bi bi-bar-chart",
+    iconActive: "bi bi-bar-chart-fill",
+    path: "/admin",
+    role: "admin",
+  },
+  {
+    name: "activity",
+    label: "Quản lý hoạt động",
+    icon: "bi bi-lightning",
+    iconActive: "bi bi-lightning-fill",
+    path: "/admin/activity",
+    role: "admin",
+  },
+  {
+    name: "reminder",
+    label: "Quản lý lịch nhắc",
+    icon: "bi bi-bell",
+    iconActive: "bi bi-bell-fill",
+    path: "/admin/reminder",
+    role: "admin",
+  },
+  {
+    name: "supplies",
+    label: "Quản lý vật tư",
+    icon: "bi bi-plug",
+    iconActive: "bi bi-plug-fill",
+    path: "/admin/supplies",
+    role: "admin",
+  },
+  {
+    name: "user",
+    label: "Quản lý tài khoản",
+    icon: "bi bi-person-circle",
+    iconActive: "bi bi-person-circle-fill",
+    path: "/admin/user",
+    role: "admin",
+  },
+];
 
 const SideBar = ({ collapsed }) => {
-  const [active, setActive] = useState("home");
+  const path = useLocation().pathname;
+  const authRole = useSelector(AuthRoleSelector);
 
   return (
     <div className={`sidebar${collapsed ? " collapsed" : ""}`}>
       <ReactSVG className="logo" src={LogoIcon} />
       <div className="menu">
-        <Link
-          to="/"
-          className={active === "home" && "active"}
-          onClick={() => setActive("home")}
-        >
-          {active === "home" ? (
-            <i className="bi bi-house-fill" />
-          ) : (
-            <i className="bi bi-house" />
-          )}
-          <span className="label">Trang chủ</span>
-        </Link>
-        <Link
-          to="/explore"
-          className={active === "activity" && "active"}
-          onClick={() => setActive("activity")}
-        >
-          {active === "activity" ? (
-            <i className="bi bi-lightning-fill" />
-          ) : (
-            <i className="bi bi-lightning" />
-          )}
-          <span className="label">Hoạt động</span>
-        </Link>
-        <Link
-          to="/calendar"
-          className={active === "calendar" && "active"}
-          onClick={() => setActive("calendar")}
-        >
-          <i className="bi bi-table" />
-          <span className="label">Lịch</span>
-        </Link>
-        <Link
-          to="/contact"
-          className={active === "contact" && "active"}
-          onClick={() => setActive("contact")}
-        >
-          {active === "contact" ? (
-            <i className="bi bi-cursor-fill" />
-          ) : (
-            <i className="bi bi-cursor" />
-          )}
-          <span className="label">Trò chuyện</span>
-        </Link>
+        {menus
+          .filter((menu) => menu.role === authRole)
+          .map((item, index) => (
+            <Link
+              key={index}
+              to={item.path}
+              className={path === item.path && "active"}
+            >
+              <i className={path === item.path ? item.iconActive : item.icon} />
+              <span className="label">{item.label}</span>
+            </Link>
+          ))}
       </div>
     </div>
   );
