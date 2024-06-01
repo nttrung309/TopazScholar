dd8ac38b34cddacc24efe2a34dc4a7a8e589f90c
ngtrhh <91332224+ngtrhh@users.noreply.github.com>
2024-06-01 22:06:02 +0700

    update

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -6,14 +6,15 @@ import activityStore from "./activity/activityStore";
 import accountManageStore from "./admin/account/accountManageStore";
 import contactStore from "./contact/contactStore";
 import hostStore from "./host/hostStore";
-import notifyStore from './notify/notifyStore';
+import notifyStore from "./notify/notifyStore";
 
 const rootReducer = combineReducers({
   user: userStore,
   activity: activityStore,
   contact: contactStore,
   accountManage: accountManageStore,
-  host: hostStore,notify: notifyStore
+  host: hostStore,
+  notify: notifyStore,
 });
 
 export default rootReducer;
